df148b6091e2f37d567ff70c1955e4eec039a754
Dani <hohoho.GG81@gmail.com>
2024-10-22 19:04:48 +0200

    Update Usage_examples.py
    Examples added

--- Usage_examples.py
+++ Usage_examples.py
@@ -1,1 +1,12 @@
+from Corona_Type_PD import corona_type_PD
 
+ctp = Corona_Type_PD()
+
+# Example of creation of a video
+ctp.make_vid.tension_variation(30000,60000,3000,3)
+
+# Example of creation of a 2D plot
+ctp.make_plots.current_density_distribution_in_axis() # Takes default values
+
+# Example of creation of a 3D plot
+ctp.make_plots.intensity_variation.conductivity(5,80,31,7,18,False)
